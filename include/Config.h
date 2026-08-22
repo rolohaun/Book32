2cfeaad98fd1cae2bb9bb09d3171fa1dfe9d79b5
rolohaun <rolohaun@users.noreply.github.com>
2026-08-22 14:06:32 -0600

    Bump Book32 to version 1.1

--- include/Config.h
+++ include/Config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // System Information
-#define SYSTEM_VERSION "1.0"
+#define SYSTEM_VERSION "1.1"
 #define DEVICE_NAME "Book32"
 
 // Offline management hotspot (SoftAP). When the device can't reach a known
